47b3a0db77c4db4ace056e7e2df5a657587b7acc
wz <wonderzombie@gmail.com>
2026-02-19 20:02:18 -0800

    remove unused implementations for Cell

--- src/cell.rs
+++ src/cell.rs
@@ -46,44 +46,9 @@ impl Cell {
     }
 }
 
-impl From<Vec2> for Cell {
-    fn from(vec: Vec2) -> Self {
-        Cell {
-            x: vec.x as i32,
-            y: vec.y as i32,
-        }
-    }
-}
-
-impl From<IVec2> for Cell {
-    fn from(vec: IVec2) -> Self {
-        Cell { x: vec.x, y: vec.y }
-    }
-}
-
-impl From<(i32, i32)> for Cell {
-    fn from(coords: (i32, i32)) -> Self {
-        Cell {
-            x: coords.0,
-            y: coords.1,
-        }
-    }
-}
-
 impl From<Cell> for (i32, i32) {
     fn from(value: Cell) -> Self {
         (value.x, value.y)
-    }
-}
-
-impl Add<Vec2> for Cell {
-    type Output = Cell;
-
-    fn add(self, rhs: Vec2) -> Cell {
-        Cell {
-            x: self.x + rhs.x as i32,
-            y: self.y + rhs.y as i32,
-        }
     }
 }
 
@@ -91,28 +56,6 @@ impl Add<IVec2> for Cell {
     type Output = Cell;
 
     fn add(self, rhs: IVec2) -> Cell {
-        Cell {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
-    }
-}
-
-impl Add<Vec3> for Cell {
-    type Output = Cell;
-
-    fn add(self, rhs: Vec3) -> Cell {
-        Cell {
-            x: self.x + rhs.x as i32,
-            y: self.y + rhs.y as i32,
-        }
-    }
-}
-
-impl Add<Cell> for Cell {
-    type Output = Cell;
-
-    fn add(self, rhs: Cell) -> Self::Output {
         Cell {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
