22ee6e34a2b085b89dbe7bda422f0938e28c30ce
wz <wonderzombie@gmail.com>
2026-02-22 15:01:33 -0800

    add View type to fov.rs to provide a RO view of MRPAS model

--- src/fov.rs
+++ src/fov.rs
@@ -11,7 +11,25 @@ use crate::{
 
 #[derive(Resource, Debug, Deref, DerefMut)]
 /// Newtype for field of view model that tracks which cells are transparent for visibility calculations.
+/// Also, Mrpas is not a Resource.
 pub struct Fov(Mrpas);
+
+#[derive(Resource, Debug, Deref, DerefMut)]
+/// Newtype for a read-only clone of an existing Mrpas model for one viewer's origin and max_distance.
+pub struct View(Mrpas);
+
+impl View {
+    pub fn new(model: &Mrpas, origin: (i32, i32), max_distance: i32) -> View {
+        let mut model = model.clone();
+        model.clear_field_of_view();
+        model.compute_field_of_view(origin, max_distance);
+        View(model)
+    }
+
+    pub fn has(&self, pos: (i32, i32)) -> bool {
+        self.0.is_in_view(pos)
+    }
+}
 
 pub fn setup_fov(
     mut commands: Commands,
@@ -22,6 +40,7 @@ pub fn setup_fov(
 
     let mut tiles_count = 0;
     let mut opaque_count = 0;
+    // Intentionally clear the field.
     fov.clear_field_of_view();
     for (cell, tile_idx) in tiles.iter() {
         fov.set_transparent(cell.into(), tile_idx.is_transparent());
@@ -39,7 +58,7 @@ pub fn setup_fov(
     commands.insert_resource(fov);
 }
 
-/// Updates the field of view model based on the transparency of tiles when their atlas index changes.
+/// Updates the field of view model based on the type of tile's transparency-or-not.
 pub fn update_fov_model(
     mut fov: ResMut<Fov>,
     query: Query<(&Cell, &TileIdx), (Changed<TileIdx>, With<MapTile>)>,
@@ -50,8 +69,9 @@ pub fn update_fov_model(
 }
 
 /// Updates the visibility of map tiles based on the player's field of view.
+/// Changes FOV because the API is stateful.
 pub fn update_fov_markers(
-    mut fov: ResMut<Fov>,
+    fov: Res<Fov>,
     player_query: Query<&Cell, With<Player>>,
     mut tiles: Query<(&Cell, &mut Revealed), With<MapTile>>,
 ) {
@@ -60,9 +80,8 @@ pub fn update_fov_markers(
         return;
     };
 
-    fov.clear_field_of_view();
-    fov.compute_field_of_view(player_cell.into(), 5);
+    let view = View::new(&fov, player_cell.into(), 5);
     for (cell, mut revealed) in tiles.iter_mut() {
-        revealed.0 = fov.is_in_view(cell.into());
+        revealed.0 = view.has(cell.into());
     }
 }
