f711bc483834809122c624161335fcf67582d763
wz <wonderzombie@gmail.com>
2026-04-18 16:47:37 -0700

    add `enum_with_str` to ldtk_loader
    works much like `tiles!`

--- src/ldtk_loader.rs
+++ src/ldtk_loader.rs
@@ -235,3 +235,37 @@ pub fn generate_ldtk_tilemap(
 
     ns.set(GameState::Loading);
 }
+
+macro_rules! enum_with_str {
+    ( $enum_name:ident, [ $( $variant:ident ),* ] ) => {
+        #[derive(Default, Debug, Eq, PartialEq, Copy, Clone, Hash, Reflect)]
+        pub enum $enum_name {
+            #[default]
+            Unset,
+            $( $variant, )*
+        }
+
+        impl $enum_name {
+            pub fn all() -> &'static [$enum_name] {
+                &[ $( $enum_name::$variant, )* ]
+            }
+
+            pub fn pairs() -> &'static [(&'static str, $enum_name)] {
+                &[ $( (stringify!($variant), $enum_name::$variant), )* ]
+            }
+
+            pub fn reverse_lookup() -> HashMap<&'static str, $enum_name> {
+                $enum_name::pairs().iter().copied().collect()
+            }
+
+            pub fn from_str(value: &str) -> Option<$enum_name> {
+                Self::pairs().iter().find(|(s, _)| &value == s).copied().map(|(_, v)| v)
+            }
+        }
+    };
+}
+
+enum_with_str!(
+    LdtkActor,
+    [Combatant, Speaker, Door, Chest, Emitter, Portal, Spawn]
+);
