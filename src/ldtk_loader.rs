59dda9fe3cf39716cfe913a978f47885455d1d04
wz <wonderzombie@gmail.com>
2026-04-19 21:39:03 -0700

    move enum with str to the top

--- src/ldtk_loader.rs
+++ src/ldtk_loader.rs
@@ -19,6 +19,32 @@ use crate::{
     tiles::{self, SHEET_SIZE_G, TileIdx},
 };
 
+macro_rules! enum_with_str {
+    ( $enum_name:ident, [ $( $variant:ident ),* ] ) => {
+        #[derive(Default, Debug, Eq, PartialEq, Copy, Clone, Hash, Reflect)]
+        pub enum $enum_name {
+            #[default]
+            Unset,
+            $( $variant, )*
+        }
+
+        #[allow(dead_code)]
+        impl $enum_name {
+            pub fn all() -> &'static [$enum_name] {
+                &[ $( $enum_name::$variant, )* ]
+            }
+
+            pub fn pairs() -> &'static [(&'static str, $enum_name)] {
+                &[ $( (stringify!($variant), $enum_name::$variant), )* ]
+            }
+
+            pub fn from_str(value: &str) -> Option<$enum_name> {
+                Self::pairs().iter().find(|(s, _)| &value == s).copied().map(|(_, v)| v)
+            }
+        }
+    };
+}
+
 pub trait LdtkEntityExt<T> {
     fn from_ldtk(entity: &LdtkEntity) -> Option<T>;
 }
@@ -298,32 +324,6 @@ pub fn generate_ldtk_tilemap(
     commands.insert_resource(spec);
 
     ns.set(GameState::Loading);
-}
-
-macro_rules! enum_with_str {
-    ( $enum_name:ident, [ $( $variant:ident ),* ] ) => {
-        #[derive(Default, Debug, Eq, PartialEq, Copy, Clone, Hash, Reflect)]
-        pub enum $enum_name {
-            #[default]
-            Unset,
-            $( $variant, )*
-        }
-
-        #[allow(dead_code)]
-        impl $enum_name {
-            pub fn all() -> &'static [$enum_name] {
-                &[ $( $enum_name::$variant, )* ]
-            }
-
-            pub fn pairs() -> &'static [(&'static str, $enum_name)] {
-                &[ $( (stringify!($variant), $enum_name::$variant), )* ]
-            }
-
-            pub fn from_str(value: &str) -> Option<$enum_name> {
-                Self::pairs().iter().find(|(s, _)| &value == s).copied().map(|(_, v)| v)
-            }
-        }
-    };
 }
 
 #[derive(Debug, Clone, Default)]
