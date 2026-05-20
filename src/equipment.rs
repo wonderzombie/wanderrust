cf771cdd51606135feaffc7e66d4bea1ebf00e88
wz <wonderzombie@gmail.com>
2026-05-19 21:18:04 -0700

    define equipment; add `modifiers!`

--- src/equipment.rs
+++ src/equipment.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::{inventory::Item, parameters::Parameters};
+use crate::{enum_with_str, inventory::Item, parameters::Parameters};
 
 #[derive(Message, Debug, Clone, Reflect)]
 pub struct Equipped {
@@ -29,5 +29,29 @@ pub(crate) struct Equippable(pub Item, pub ParamsModifiers);
 impl Equippable {
     pub fn modify(&self, params: Parameters) -> Parameters {
         params + self.1.0
+    }
+}
+
+enum_with_str!(Equipment, [Stick, Rags, Leather, Chainmail, Shield]);
+
+macro_rules! modifiers {
+    ( $( $fieldn:tt = $fieldv:expr )* $(,)? ) => {
+        ParamsModifiers(Parameters {
+            $( $fieldn: $fieldv, )*
+            ..default()
+        })
+    };
+}
+
+impl Equipment {
+    pub(crate) fn modifiers(&self) -> ParamsModifiers {
+        match self {
+            Equipment::Unset => ParamsModifiers::default(),
+            Equipment::Stick => modifiers!(attack = 1),
+            Equipment::Rags => modifiers!(defense = 1),
+            Equipment::Leather => modifiers!(defense = 3),
+            Equipment::Chainmail => modifiers!(defense = 5),
+            Equipment::Shield => modifiers!(defense = 2),
+        }
     }
 }
