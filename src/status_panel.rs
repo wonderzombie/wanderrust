85e0749fb640726796605423d2e98f630f3a063f
wz <wonderzombie@gmail.com>
2026-08-12 15:22:33 -0700

    add more data to status panel

--- src/status_panel.rs
+++ src/status_panel.rs
@@ -1,10 +1,13 @@
-use bevy::prelude::*;
+use bevy::{platform::collections::HashMap, prelude::*};
 
 use crate::{
     actors::{Flasks, Player},
     cell::Cell,
     colors,
+    equipment::{EquippedBy, HasEquipped, unwrap_collection},
     gamestate::{Screen, WorldClock},
+    inventory::Inventory,
+    items::{ItemId, Slot},
     parameters::{Health, Parameters},
     ui::theme,
 };
@@ -26,14 +29,36 @@ pub enum Label {
     Flasks,
     Ticks,
     Cell,
+    Move,
+    Attack,
+    Defense,
+    MainHand,
+    OffHand,
+    Armor,
+    Trinket,
+    Gold,
 }
 
 fn update_labels(
-    status: Single<(&Cell, &Health, &Flasks), With<Player>>,
+    status: Single<(&Cell, &Health, &Flasks, &Parameters, Option<&HasEquipped>), With<Player>>,
+    equipment: Query<(&ItemId, &EquippedBy)>,
+    inv: Res<Inventory>,
     mut labels: Query<(&mut Text, &Label)>,
     clock: Res<WorldClock>,
 ) {
-    let (cell, health, flasks) = *status;
+    let (cell, health, flasks, params, equipment_opt) = *status;
+
+    let equip_nt: Vec<Entity> = unwrap_collection(equipment_opt);
+    let equipped = equipment
+        .iter_many(equip_nt)
+        .map(|(it, eq_by)| (eq_by.slot, it.def().label))
+        .collect::<HashMap<Slot, &str>>();
+    let eq_or_none = |s: Slot| equipped.get(&s).unwrap_or_else(|| &"--").to_uppercase();
+
+    let g = inv
+        .item_quantity(&ItemId::Gold)
+        .map(|it| it.0)
+        .unwrap_or_default();
 
     for (mut text, label) in labels.iter_mut() {
         let new_text = match label {
@@ -41,6 +66,14 @@ fn update_labels(
             Label::Flasks => format!("FR: {}", flasks.0),
             Label::Ticks => format!("T:  {}", *clock),
             Label::Cell => format!("C:  {}", *cell),
+            Label::Move => format!("M: S{}", params.move_speed),
+            Label::Attack => format!("A: {} S{}", params.attack, params.attack_speed),
+            Label::Defense => format!("D: {}", params.defense),
+            Label::MainHand => format!("+ {}", eq_or_none(Slot::MainHand)),
+            Label::OffHand => format!("+ {}", eq_or_none(Slot::OffHand)),
+            Label::Armor => format!("+ {}", eq_or_none(Slot::Armor)),
+            Label::Trinket => format!("+ {}", eq_or_none(Slot::Trinket)),
+            Label::Gold => format!("{} GP", g),
         };
         text.set_if_neq(Text::new(new_text));
     }
@@ -86,6 +119,8 @@ pub(crate) fn scene() -> impl Scene {
                 theme::pcsr_font(16)
                 TextColor(colors::KENNEY_OFF_WHITE)
             ,
+                Node { height: px(16) }
+            ,
                 theme::label_row("HP: ??")
                 Label::Hp
             ,
@@ -98,6 +133,35 @@ pub(crate) fn scene() -> impl Scene {
                 theme::label_row("C: ??")
                 Label::Cell
             ,
+                Node { height: px(16) }
+            ,
+                theme::label_row("")
+                Label::Move
+            ,
+                theme::label_row("")
+                Label::Attack
+            ,
+                theme::label_row("")
+                Label::Defense
+            ,
+                Node { height: px(16) }
+            ,
+                theme::label_row("")
+                Label::MainHand
+            ,
+                theme::label_row("")
+                Label::OffHand
+            ,
+                theme::label_row("")
+                Label::Armor
+            ,
+                theme::label_row("")
+                Label::Trinket
+            ,
+                Node { height: px(16) }
+            ,
+                theme::label_row("?? GP")
+                Label::Gold
         ]
     }
 }
